c0cd3e25eb00127922e387e516aa90cb86dec08b
jaden <nguyenthucanh422@gmail.com>
2023-10-19 19:39:02 -0400

    reviews to be done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  # We need to find the restaurant associated with the review
+  before_action :set_restaurant, only: %i[new create]
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
